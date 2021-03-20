b0b323bc893292e74f3ac60e8c4172bf90319584
Isaac Abrego <abregoisaac01@gmail.com>
2021-03-20 12:09:35 -0600

    postgresql setup completed, user created to access db, rest_framework was configured as well

--- backend/medflow/settings.py
+++ backend/medflow/settings.py
@@ -37,6 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'rest_framework',
+    "rest_framework_api_key",
 ]
 
 MIDDLEWARE = [
@@ -75,8 +77,12 @@ WSGI_APPLICATION = 'medflow.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'medflow',
+        'USER': 'medflow_admin',
+        'PASSWORD': 'Medflow123',
+        'HOST': '127.0.0.1',
+        'DATABASE_PORT': '5432',
     }
 }
 
